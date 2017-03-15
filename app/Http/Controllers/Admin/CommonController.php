37215e8dc3f116b2492c69d3fb8e361843ea1596
Tealy <1131180370@qq.com>
2017-03-15 22:36:02 +0800

    file uploads

--- app/Http/Controllers/Admin/CommonController.php
+++ app/Http/Controllers/Admin/CommonController.php
@@ -12,7 +12,16 @@ class CommonController extends Controller
     //图片上传
     public function upload()
     {
-        $input = Input::all();
-        dd($input);
+        $file = Input::file('Filedata');dd($file);
+        if( $file->isValid ){
+            $realPath = $file->getRealPath();  //临时文件的绝对路径
+            $entension = $file->getClientOriginalExtension();  //上传文件的后缀
+            $newName = date('YmdHis', time()).mt_rand(100, 999).'.'.$entension;
+            $path = $file->move( base_path().'/uploads', $newName );
+            $filepath = 'uploads/'.$newName;
+
+            return $filepath;
+        }
+
     }
 }
